23235e640e36ab1ef1d5ba698ce18ee0efa3c59f
Javier P <javierp@neftyblocks.com>
2022-08-15 14:59:51 +0200

    Add sample tests for integration test

---
+++ test/typescript/integration.test.ts
@@ -0,0 +1,9 @@
+import { NCO_BlockchainAPI, devnet_services, devnet_urls } from '../..';
+
+
+test('it should perform a test call correctly', async () => {
+    const api = new NCO_BlockchainAPI(devnet_urls, devnet_services);
+    const keyPair = await api.createKeyPair();
+    expect(keyPair.prv_key).toBeDefined();
+    expect(keyPair.pub_key).toBeDefined();
+})
