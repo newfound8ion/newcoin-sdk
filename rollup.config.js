c7afc1512430b2015d052ada223c9ba54476f809
Javier P <javierp@neftyblocks.com>
2022-08-16 14:51:33 +0200

    Modify extensions for commonjs and module builds

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,12 @@ export default [
     external: ['eosjs', 'eosjs/dist/eosjs-jssig', '@eoscafe/hyperion', '@newcoin-foundation/newcoin.pools-js', '@newcoin-foundation/newcoin.pool-js', '@newcoin-foundation/newcoin.daos-js', 'cross-fetch'],
     output: [
       {
-      file: "./dist/index.esm.js",
+      file: "./dist/index.mjs",
       format: "esm",
       sourcemap: true,
     },
     {
-      file: "./dist/index.js",
+      file: "./dist/index.cjs",
       format: "cjs",
       sourcemap: true,
     }],
