dd15538f64a8a032a03768db79f79e4326553eea
vi-newcoin <94545292+vi-newcoin@users.noreply.github.com>
2022-09-21 20:42:35 +0300

    file modification - nft only

--- src/index.ts
+++ src/index.ts
@@ -888,9 +888,9 @@ export class NCO_BlockchainAPI {
           console.log("Second Minting error message:  " + err);
         }
 
-        return -1;
-
       }; 
+
+      throw "Double error";
 
     }
 
@@ -898,24 +898,25 @@ export class NCO_BlockchainAPI {
     {
       const old = await readAsset(inpt.asset_id);
       console.log("changing file : ... " + JSON.stringify(old.mutable_data));
-      let data = { 
-        name:    inpt.new_name ?? old.mutable_data.name,
-        path:    inpt.new_path?? old.mutable_data.path,
-        content: inpt.new_content?? old.mutable_data.content,
-        image:   inpt.new_image??old.mutable_data.image
-      };
+      let data = [
+        {'key': 'name', 'value': ['string', inpt.new_name ?? old.mutable_data.name]},
+        {'key': 'path','value': ['string', inpt.new_path?? old.mutable_data.path]}, 
+        {'key': 'content','value': ['string',  inpt.new_content?? old.mutable_data.content]},
+        {'key': 'image','value': ['string',  inpt.new_image??old.mutable_data.image ]}
+    ];
 
       let n : NCModifyAsset = {
         editor: inpt.editor,
         owner: inpt.editor,
         asset_id: inpt.asset_id,
-        new_data: data as any,
+        new_data: data,
         payer: inpt.payer??inpt.editor,
         payer_prv_key: inpt.payer_prv_key
 
       };
       let res = await this.modifyAsset(n);
       res.TxID_changeFile=res.TxID_modifyAsset;
+      res.asset_id = inpt.asset_id;
       console.log("modify asset res: "+ JSON.stringify(res));
       return res;
     }
